da440847da3eb331016bc4683bc483b0c8909507
MOONKYU401 <mmk6553@gmail.com>
2025-04-21 15:49:56 -0600

    create weather

--- weather.js
+++ weather.js
@@ -9,39 +9,4 @@ const connectDB = async () => {
   }
 };
 
-connectDB();
-
-const WeatherSchema = new mongoose.Schema({
-  date: {
-    type: Date,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  weatherType: {
-    type: String,
-    enum: [
-      'Thunderstorm', 'Drizzle', 'Rain', 'Snow', 'Clear', 'Clouds',
-      'Mist', 'Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash',
-      'Squall', 'Tornado'
-    ],
-    required: true,
-  },
-  temperature: {
-    type: Number, // In Celsius
-    required: true,
-  },
-  description: {
-    type: String, // e.g., "light rain", "few clouds"
-  },
-  humidity: {
-    type: Number, // Percentage (0–100)
-  },
-  windSpeed: {
-    type: Number, // In m/s
-  },
-});
-
 module.exports = mongoose.model('Weather', WeatherSchema);
