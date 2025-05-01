c770ef9e4f144fff5315800ec15f0ad351901251
MOONKYU401 <mmk6553@gmail.com>
2025-04-30 23:09:02 -0600

    api delete

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", authRoutes);
-app.use("/api/history", historyRoutes);
+app.use("/auth", authRoutes);
+app.use("/history", historyRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
